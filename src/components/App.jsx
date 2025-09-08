bc7d3c6e5718d4ba0ea4d221d8aaf5c38beebb4c
Mehdi Raza <mehdics100@gmail.com>
2025-09-08 18:51:59 +0500

    Introduced local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  const [Notes, setNotes] = useState([]);
+  const [Notes, setNotes] = useState(() => {
+    const savedNotes = localStorage.getItem("notes");
+    return savedNotes ? JSON.parse(savedNotes) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(Notes));
+  }, [Notes]);
+
   function addNote(note) {
     setNotes((previous) => [...previous, note]);
   }
@@ -13,6 +21,7 @@ function App() {
   function deleteNote(id) {
     setNotes((previous) => previous.filter((element, index) => index !== id));
   }
+
   return (
     <div>
       <Header />
